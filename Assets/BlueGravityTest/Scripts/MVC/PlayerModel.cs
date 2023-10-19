5021633d14aeb8109515ee1042679a61c257fbbb
Mallo-Lucas <61807778+Mallo-Lucas@users.noreply.github.com>
2023-10-19 14:01:01 -0300

    animations

--- Assets/BlueGravityTest/Scripts/MVC/PlayerModel.cs
+++ Assets/BlueGravityTest/Scripts/MVC/PlayerModel.cs
@@ -1,3 +1,4 @@
+using System;
 using BlueGravityTest.ScriptableObjects.Player;
 using UnityEngine;
 
@@ -5,8 +6,11 @@ namespace BlueGravityTest.Scripts.MVC
 {
     public class PlayerModel : MonoBehaviour
     {
+        public Action<Vector2> OnMove;
+        
         [SerializeField] private PlayerData playerData;
         [SerializeField] private PlayerController controller;
+        [SerializeField] private PlayerView view;
         [SerializeField] private Rigidbody2D rb;
 
         private Vector2 _movementDirection;
@@ -31,11 +35,13 @@ namespace BlueGravityTest.Scripts.MVC
         private void Move()
         {
             rb.MovePosition((Vector2)transform.position + _movementDirection * (_movementSpeed * Time.deltaTime));
+            OnMove?.Invoke(_movementDirection);
         }
         
         private void Subscribe()
         {
             controller.OnMove += SetMovementDirection;
+            view.Subscribe(this);
         }
 
         private void InitializeData()
--- Assets/BlueGravityTest/Scripts/MVC/PlayerView.cs
+++ Assets/BlueGravityTest/Scripts/MVC/PlayerView.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerView : MonoBehaviour
+namespace BlueGravityTest.Scripts.MVC
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerView : MonoBehaviour
     {
+        public enum BodyParts {Body, Shirt, Shoes, Pants}
+
         
+        [SerializeField] private List<AnimatorParts> playerAnimators;
+
+        private static readonly string MOVE_ANIMATION ="OnMove";
+        private static readonly string X_MAGNITUDE ="X";
+        private static readonly string Y_MAGNITUDE ="Y";
+        
+        public void Subscribe(PlayerModel model)
+        {
+            model.OnMove += MoveAnimation;
+        }
+
+        private void MoveAnimation(Vector2 dir)
+        {
+            if (dir == Vector2.zero)
+            {
+                foreach (var animatorParts in playerAnimators)
+                    animatorParts.animator.SetBool(MOVE_ANIMATION, false);
+                return;
+            }
+            
+            foreach (var animatorParts in playerAnimators)
+            {
+                animatorParts.animator.SetBool(MOVE_ANIMATION, true);
+                animatorParts.animator.SetFloat(X_MAGNITUDE, dir.x);
+                animatorParts.animator.SetFloat(Y_MAGNITUDE, dir.y);
+                animatorParts.spriteRenderer.flipX = !(dir.x > 0);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public class AnimatorParts
     {
-        
+        public PlayerView.BodyParts bodyParts;
+        public Animator animator;
+        public SpriteRenderer spriteRenderer;
     }
 }
+
